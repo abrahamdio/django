5b61e23e6c1ef97ead4cd0449c6247f755f362ad
Dio <abrahamdio@gmail.com>
2015-07-19 10:40:56 +0000

    static deployment bugfix

--- lwc/settings.py
+++ lwc/settings.py
@@ -100,5 +100,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+PROJECT_DIR  = os.path.dirname(__file__)
+
+MEDIA_ROOT = os.path.join(PROJECT_DIR,'media')
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR, 'staticfiles'),
+)
--- lwc/urls.py
+++ lwc/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 from django.conf.urls import include, url, patterns
 from django.contrib import admin
 from lwc import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^$', 'lwc.views.index', name='index'),
 	url(r'^home/', 'joins.views.home', name='home'),
 	# url(r'^home/', 'joins.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
